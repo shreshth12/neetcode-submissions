b54a08ba2e89fb97ff999da83f28d0e3efd255d5
Shreshth Kaushik <shreshthk185796@gmail.com>
2026-04-21 13:24:38 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 1
+print(type(variable))
+
+variable = 1.1
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "1"
+print(type(variable))
+
+variable = [1]
+print(type(variable))
