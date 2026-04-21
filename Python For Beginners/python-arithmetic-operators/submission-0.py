50dd63e227bed48e3281f45a80496d27a3c8dcab
Shreshth Kaushik <shreshthk185796@gmail.com>
2026-04-21 14:49:28 -0400

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,10 @@
+a, b, c = 2, 2, 0.5
+total = a + b + c
+zero_minus_total = 0 - total
+product = a * b * c
+sum_div_product = total / product
+
+print(total)
+print(zero_minus_total)
+print(product)
+print(sum_div_product)
